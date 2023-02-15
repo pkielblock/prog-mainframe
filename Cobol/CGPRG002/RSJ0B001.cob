5348415c499d2e388d690c8f7164f58ed68672c9
Pedro Kielblock <p.kielblock123@gmail.com>
2023-02-15 17:37:28 -0300

    Second program in cobol

---
+++ Cobol/CGPRG002/RSJ0B001.cob
@@ -0,0 +1,6 @@
+//GPAZ99J1 JOB SATOSHI,MSGCLASS=X,CLASS=C,NOTIFY=GPAZ99,TIME=(0,20)
+//*****************************************************************
+//STEP1   EXEC PGM=CGPRG002
+//STEPLIB DD DSN=GP.GERAL.LOADLIB,DISP=SHR
+//SYSIN   DD *
+//*
