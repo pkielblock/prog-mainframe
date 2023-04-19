c68fe53b49e64ccc724ff4123b85addd6776c4a4
Pedro Kielblock <p.kielblock123@gmail.com>
2023-04-19 15:50:29 -0300

    PRG 006

--- Files/CGPRG006.cob
+++ Files/CGPRG006.cob
@@ -1,10 +1,10 @@
        IDENTIFICATION DIVISION.
       *=======================*
        PROGRAM-ID.   CGPRG006.
-       AUTHOR.       PEDRO.
+       AUTHOR.       PEDRO KIELBLOCK.
        INSTALLATION. FATEC SAO CAETANO.
-       DATE-WRITTEN. 29/03/2023.
-       DATE-COMPILED. 29/03/2023.
+       DATE-WRITTEN. 03/09/2019.
+       DATE-COMPILED. 05/04/2023.
       *--------------------------------------------------------------*
        ENVIRONMENT DIVISION.
       *====================*
@@ -16,25 +16,38 @@
            .
        INPUT-OUTPUT SECTION.
       *---------------------*
-       FILE-CONTROL.
-      *==> LOCAL PARA O SELECT DOS ARQUVOS
-
        DATA DIVISION.
       *=============*
        FILE SECTION.
       *------------*
-      *==> LOCAL PARA A FD (DESCRICAO DOS ARQUIVOS)
-
        WORKING-STORAGE SECTION.
       *-----------------------*
+       01  FILLER                 PIC X(35)        VALUE
+           '**** INICIO DA WORKING-STORAGE ****'.
 
-      *-----> AREA AUXILIAR
+      *-----> VARIAVEIS AUXILIARES UTILIZADA NO PROCESSAMENTO
        01  WS-AREA-AUX.
-           05  WS-FIM                 PIC X(01) VALUE "N".
-           05  WS-CTLIDO              PIC 9(02).
-           05  WS-DATA                PIC 9(08).
-           05  WS-AUXPCTACID          PIC 9(4)V99.
-      *-----> DADOS DE ENTRADA
+           05  WS-FIM                 PIC X(01).
+           05  WS-RGLIDO              PIC 9(02).
+           05  WS-DATA                PIC 9999/99/99.
+           05  WS-PORC                PIC 99V9999.
+           05  WS-SOMA-SP             PIC 99V9999.
+           05  WS-CONT-SP             PIC 9(02).
+           05  WS-MEDIA-SP            PIC 99V9999.
+           05  MEDIA-SP               PIC ZZ9,99.
+           05  WS-QTD-ASP             PIC 9(05).
+           05  WS-QTD-CSP             PIC 99.
+           05  QTD-ACSP               PIC ZZ.ZZ9.
+           05  WS-CID-MAIOR           PIC 9(05).
+           05  WS-QTD-MAIOR-CID       PIC 9(04).
+           05  QTD-MAIOR-CID          PIC Z.ZZ9.
+           05  WS-CID-MENOR           PIC 9(05).
+           05  WS-M-PORC              PIC 99V9999.
+           05  M-PORC                 PIC ZZ9,99.
+           05  WS-QTD-M-OBITO         PIC 9(04).
+           05  WS-QTD-M-ACID          PIC 9(05).
+
+      *-----> ENTRADA - DADOS VIA SYSIN (NO JCL DE EXECUCAO)
        01  WS-REG-SYSIN.
            05 WS-CIDADE           PIC 9(05).
            05 WS-ESTADO           PIC X(2).
@@ -42,71 +55,136 @@
            05 WS-BAFOMETRO        PIC X(01).
            05 WS-QTD-ACIDENTES    PIC 9(04).
            05 WS-QTD-OBITOS       PIC 9(04).
-      *-----> DADOS DE SAIDA VIA SYSOUT
-       01  WS-REG-SYSOUT.
-           05 CID                 PIC 99999.
-           05 FILLER              PIC X(01) VALUE '-'.
-           05 UF                  PIC XX.
-           05 FILLER              PIC X(04) VALUE SPACES.
-           05 QTVEICS             PIC Z.ZZZ.ZZ9.
-           05 FILLER              PIC X(04) VALUE SPACES.
-           05 BAFO                PIC X.
-           05 FILLER              PIC X(04) VALUE SPACES.
-           05 QTACIDS             PIC Z.ZZ9.
-           05 FILLER              PIC X(04) VALUE SPACES.
-           05 QTOBITOS            PIC Z.ZZ9.
-           05 FILLER              PIC X(04) VALUE SPACES.
-           05 PACIDS              PIC ZZ9,99.
-           05 FILLER              PIC X(01) VALUE '%'.
 
-       LINKAGE SECTION.
-      *----------------*
-       01  LK-PARAMETROS.
-           05 LK-NR-DPTO             PIC 9(04).
-           05 LK-NOME-DPTO           PIC X(15).
-           05 LK-COD-RETORNO         PIC 99.
+      *----> SAIDA DE DADOS - VIA SYSOUT
+       01  WS-REG-SYSOUT.
+           05 CID                  PIC 99999.
+           05 FILLER               PIC X(01)       VALUE "-".
+           05 UF                   PIC XX.
+           05 FILLER               PIC X(04)       VALUE SPACES.
+           05 QTVEICS              PIC Z.ZZZ.ZZ9.
+           05 FILLER               PIC X(04)       VALUE SPACES.
+           05 BAFO                 PIC X.
+           05 FILLER               PIC X(04)       VALUE SPACES.
+           05 QTACIDS              PIC Z.ZZ9.
+           05 FILLER               PIC X(04)       VALUE SPACES.
+           05 QTOBITOS             PIC Z.ZZ9.
+           05 FILLER               PIC X(04)       VALUE SPACES.
+           05 PACIDS               PIC Z9,99.
+           05 FILLER               PIC X(01)       VALUE "%".
       *
        PROCEDURE DIVISION.
       *==================*
       *--------------------------------------------------------------*
-      *    PROCESSO PRINCIPAL                                        *
+      *    PROCESSO PRINCIPAL
       *--------------------------------------------------------------*
+       000-CGPRG006.
 
            PERFORM 010-INICIAR
            PERFORM 030-PROCESSAR UNTIL WS-FIM = 'S'
-           PERFORM 050-TERMINAR
+           PERFORM 040-PROCESSAR-SP
+           PERFORM 045-PROCESSAR-MAIOR
+           PERFORM 047-PROCESSAR-MENOR
+           PERFORM 090-TERMINAR
            STOP RUN
            .
       *--------------------------------------------------------------*
-      *    PROCEDIMENTOS INICIAIS                                    *
+      *    PROCEDIMENTOS INICIAIS
       *--------------------------------------------------------------*
        010-INICIAR.
-
-           ACCEPT WS-DATA FROM DATE
-           DISPLAY "PEDRO"
+           ACCEPT  WS-DATA FROM DATE
+           DISPLAY "PEDRO KIELBLOCK"
            DISPLAY "ATIVIDADE 6"
-           DISPLAY "ESTATISTICAS - DATA DO CALCULO:" WS-DATA (7:2) "/"
-               WS-DATA (5:2) "/" "2" WS-DATA (2:3)
-           MOVE  ZEROS  TO  WS-CTLIDO
+           DISPLAY "ESTATISTICAS - DATA DO CALCULO: " WS-DATA(9:2) "/"
+                      WS-DATA(6:2) "/2" WS-DATA(2:3)
+           DISPLAY "-------------------------------------------"
+           PERFORM 025-LER-SYSIN
            .
       *--------------------------------------------------------------*
-      *    PROCESSAR DADOS RECEBIDOS DA SYSIN                        *
+      *    LEITURA DADOS DA SYSIN
+      *--------------------------------------------------------------*
+       025-LER-SYSIN.
+
+           ACCEPT WS-REG-SYSIN  FROM SYSIN
+
+           IF WS-REG-SYSIN = ALL '9'
+              MOVE   'S'     TO  WS-FIM
+           ELSE
+              ADD 1  TO WS-RGLIDO
+           END-IF
+           .
+      *--------------------------------------------------------------*
+      *    PROCESSAR DADOS RECEBIDOS DA SYSIN ATE FIM DOS REGISTROS
       *--------------------------------------------------------------*
        030-PROCESSAR.
-           ACCEPT WS-REG-SYSIN FROM SYSIN
-           IF WS-REG-SYSIN = ALL '9'
-              MOVE 'S' TO WS-FIM
-           ELSE
-              ADD 1 TO WS-CTLIDO
+
+           COMPUTE WS-PORC = (WS-QTD-ACIDENTES / WS-QTD-VEICULOS) * 100
+           MOVE WS-PORC            TO  PACIDS
+           MOVE WS-CIDADE          TO  CID
+           MOVE WS-ESTADO          TO  UF
+           MOVE WS-QTD-VEICULOS    TO  QTVEICS
+           MOVE WS-BAFOMETRO       TO  BAFO
+           MOVE WS-QTD-ACIDENTES   TO  QTACIDS
+           MOVE WS-QTD-OBITOS      TO  QTOBITOS
+           IF WS-ESTADO = 'SP'
+              ADD WS-QTD-ACIDENTES TO WS-QTD-ASP
+              ADD 1       TO WS-QTD-CSP
+              ADD WS-PORC TO WS-SOMA-SP
+              ADD 1       TO WS-CONT-SP
            END-IF
-           COMPUTE WS-AUXPCTACID = WS-QTD-VEICULOS / WS-QTD-ACIDENTES
-           MOVE WS-AUXPCTACID TO PACIDS
-           MOVE WS-CIDADE TO CID
-           MOVE WS-ESTADO TO UF
-           MOVE WS-QTD-VEICULOS TO QTVEICS
-           MOVE WS-BAFOMETRO TO BAFO
-           MOVE WS-QTD-ACIDENTES TO QTACIDS
-           MOVE WS-QTD-OBITOS TO QTOBITOS
+           IF WS-RGLIDO = 1
+              MOVE WS-QTD-ACIDENTES TO WS-QTD-MAIOR-CID
+              MOVE WS-CIDADE        TO WS-CID-MAIOR
+              MOVE WS-CIDADE        TO WS-CID-MENOR
+              MOVE WS-PORC          TO WS-M-PORC
+           END-IF
+           IF WS-QTD-ACIDENTES > WS-QTD-MAIOR-CID
+              MOVE WS-QTD-ACIDENTES TO WS-QTD-MAIOR-CID
+              MOVE WS-CIDADE        TO WS-CID-MAIOR
+           END-IF
+           IF WS-PORC < WS-M-PORC
+              MOVE WS-CIDADE        TO WS-CID-MENOR
+              MOVE WS-PORC          TO WS-M-PORC
+              MOVE WS-QTD-OBITOS    TO WS-QTD-M-OBITO
+              MOVE WS-QTD-ACIDENTES TO WS-QTD-M-ACID
+           END-IF
            DISPLAY WS-REG-SYSOUT
+           PERFORM 025-LER-SYSIN
            .
-      *---------------> FIM DO PROGRAMA CGRPG006 <-------------------*
+       040-PROCESSAR-SP.
+
+            COMPUTE WS-MEDIA-SP = WS-SOMA-SP / WS-CONT-SP
+            MOVE WS-MEDIA-SP TO MEDIA-SP
+            MOVE WS-QTD-ASP  TO QTD-ACSP
+            DISPLAY '---------------------------------'
+            DISPLAY 'MEDIA DAS PROCENTAGENS DE SP.....: ' MEDIA-SP "%"
+            DISPLAY 'QTD. DE ACIDENTES TOTAIS DE SP...: ' QTD-ACSP
+            DISPLAY 'QTD. DE CIDADES DE SP PESQUISADAS: ' WS-QTD-CSP
+            .
+       045-PROCESSAR-MAIOR.
+            MOVE WS-QTD-MAIOR-CID TO QTD-MAIOR-CID
+            DISPLAY '---------------------------------'
+            DISPLAY 'CIDADE COM MAIOR QTD DE ACIDENTES: ' WS-CID-MAIOR
+            DISPLAY 'QTD. DE ACIDENTES DESTA CIDADE...: '
+               QTD-MAIOR-CID
+            DISPLAY 'QTD. DE CIDADES PESQUISADAS......: ' WS-RGLIDO
+            .
+       047-PROCESSAR-MENOR.
+            COMPUTE WS-M-PORC = (WS-QTD-M-OBITO / WS-QTD-M-ACID) * 100
+            MOVE WS-M-PORC TO M-PORC
+            DISPLAY '----------------------------------'
+            DISPLAY 'CIDADE COM MENOR PORCENTAGEM DE OBITOS: '
+                     WS-CID-MENOR
+            DISPLAY 'PORCENTAGEM DE OBITOS DESTA CIDADE....: '
+                     M-PORC '%'
+            .
+      *--------------------------------------------------------------*
+      *    PROCEDIMENTOS FINAIS
+      *--------------------------------------------------------------*
+       090-TERMINAR.
+
+           DISPLAY ' *========================================*'
+           DISPLAY ' *        FIM DO PROGRAMA CGPRG006           *'
+           DISPLAY ' *----------------------------------------*'
+           .
+      *---------------> FIM DO PROGRAMA CGPRG006 <-------------------*
